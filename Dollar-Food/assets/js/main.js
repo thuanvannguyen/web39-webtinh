7b1f4f640907fb44fe4c28f40ca0598aab2dbdb1
ThuanVan Nguyen <60531224+Nguyen-Van-Thuan@users.noreply.github.com>
2024-06-01 19:02:35 +0700

    update b3

--- Dollar-Food/assets/js/main.js
+++ Dollar-Food/assets/js/main.js
@@ -3,9 +3,10 @@ $(document).ready(function () {
   $('.owl-carousel').owlCarousel({
     loop: true,
     margin: 10,
-    nav: true,
+    nav: false,
     autoplay: true,
     autoplayTimeout: 3000,
+    dots: false,
     responsive: {
       0: {
         items: 1
